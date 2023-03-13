848a55990ced17123e63c2d792fb65cfb5915e18
Kodisang <br.kodisang@gmail.com>
2023-03-13 12:42:16 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 
+"""
+This module contains a function replace_in_list that replaces an element
+in a list at a specific position.
+"""
 
-def print_reversed_list_integer(my_list=[]):
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replaces an element of a list at a specific position.
+
+    Args:
+        my_list (list): The list to modify.
+        idx (int): The index of the element to replace.
+        element (int): The new element to replace with.
+
+    Returns:
+        The modified list if idx is within the range of the list,
+        otherwise the original list.
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+
+    my_list[idx] = element
+    return my_list
